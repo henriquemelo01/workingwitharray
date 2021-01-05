ccd9daa3652cad3044f9f6811c0ceefb28647a91
Henrique Moraes <henriquemsm@outlook.com>
2021-01-05 14:46:02 -0300

    Challenge #2

--- script.js
+++ script.js
@@ -445,7 +445,7 @@ console.log(retiradas);
 
 const balance = movements.reduce(function (acc, cur, i, arr) {
   // console.log(`Itineration ${i}: ${acc}`);
-  return acc + cur;
+  return acc + cur; // updating acumulator: acc = acc + cur;
 }, 0);
 
 console.log(balance);
@@ -465,7 +465,62 @@ const maxMovValue = movements.reduce(function (acc, currentValue, i) {
   // Acumulador será responsável por rastrear o nosso valor maximo:
   console.log(acc, i);
   if (acc > currentValue) return acc;
-  else return currentValue; // currentValue será o novo acc
-});
+  else return currentValue; // currentValue será o novo acc (atualiza o valor do acumulador)
+}, movements[0]);
 
 console.log(maxMovValue);
+
+// ------------------ Coding challenge #2 ------------------
+
+/*
+
+Let's go back to Julia and Kate's study about dogs. This time, they want to convert dog ages to human ages and calculate the average age of the dogs in their study.
+
+Your tasks:
+Create a function 'calcAverageHumanAge', which accepts an arrays of dog's ages ('ages'), and does the following things in order:
+1. Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4 - map 
+2. Exclude all dogs that are less than 18 human years old (which is the same as - keeping dogs that are at least 18 years old)
+
+3. Calculate the average human age of all adult dogs (you should already know from other challenges how we calculate averages �) - reduce 
+
+4. Run the function for both test datasets
+Test data:
+§ Data 1: [5, 2, 4, 1, 15, 8, 3]
+§ Data 2: [16, 6, 10, 5, 6, 1, 4]
+
+
+
+*/
+
+const juliaDogs = [5, 2, 4, 1, 15, 8, 3];
+const kateDogs = [16, 6, 10, 5, 6, 1, 4];
+
+const calcAverageHumanAge = function (dogsAges) {
+  // Calculate the dog age in human years using the following formula: if the dog is <= 2 years old, humanAge = 2 * dogAge. If the dog is > 2 years old, humanAge = 16 + dogAge * 4
+
+  // Como vamos precisar excluir os cachorros que não são adultos, vamos criar um  novo vetor que armazena todas as médias calculadas e usar o metodo filter();
+
+  const humansAges = dogsAges.map(function (dogAge, index) {
+    if (dogAge <= 2) {
+      return 2 * dogAge;
+    } else {
+      return 16 + dogAge * 4;
+    }
+  });
+
+  // 2. Excluindo os cachorros que não são adultos:
+  const adults = humansAges.filter(humansAge => humansAge >= 18);
+
+  // Calculo Media das Idade dos cães adultos: Usando reduce para somar todos os valores do vetor adults, .length para calcular o seu de número de elementos -> Media = somaElementos/nº elementos
+
+  const averageAge =
+    adults.reduce(function (acc, adult) {
+      return acc + adult; // atualiza accumulator
+    }, 0) / adults.length;
+
+  return averageAge;
+};
+
+console.log('\n\n------------- Chalenge #2 -------------');
+console.log(`Average 1: ${calcAverageHumanAge(juliaDogs)}`);
+console.log(`Average 1: ${calcAverageHumanAge(kateDogs)}`);
